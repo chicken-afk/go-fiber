068db17588170c0305651461cedda99634d6a5b9
chicken-afk <andikayuda628@gmail.com>
2023-11-20 13:35:49 +0700

    middleware

--- routes/route.go
+++ routes/route.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	"github.com/chicken-afk/go-fiber/pkg/controllers"
+	"github.com/chicken-afk/go-fiber/pkg/middleware"
 	"github.com/gofiber/fiber/v2"
 )
 
@@ -9,10 +10,11 @@ func RouteInit(route *fiber.App) {
 	//Prefix
 	api := route.Group("/api")
 	v1 := api.Group("/v1")
+
 	// Routes
 	UserController := controllers.NewUserController()
-	v1.Get("/user", UserController.Index)
-	v1.Post("/user", UserController.Create)
+	v1.Get("/user", middleware.AuthMiddleware, UserController.Index)
+	v1.Post("/user", middleware.AuthMiddleware, UserController.Create)
 	v1.Get("/user/:id", UserController.Detail)
 	v1.Put("user/:id", UserController.Update)
 	v1.Delete("user/:id", UserController.DeleteUser)
